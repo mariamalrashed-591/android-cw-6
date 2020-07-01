3965b0d2855e75c2a45be61aefa6a1a94d171543
mariamalrashed-591 <66645992+mariamalrashed-591@users.noreply.github.com>
2020-07-01 17:48:04 +0300

    cw 6 done Mariam AlRashed 591

---
+++ day6/app/src/main/java/com/example/day6/Movie.java
@@ -0,0 +1,10 @@
+package com.example.day6;
+
+public class Movie {
+
+    private String title;
+    private String mainActor;
+    private String genre;
+    private int pgRate;
+    private double movieRate;
+}
